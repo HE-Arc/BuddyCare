88b4710934bdde389311a60cac3b6769f8254656
Spike1953 <kevin.baland@he-arc.ch>
2015-03-05 16:18:45 +0000

    Ajout popup

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   resources :messages
 
   resources :users
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  get '/login' => 'user#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
