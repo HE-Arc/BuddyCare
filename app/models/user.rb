82ea9585c719497f57952bd205b9230473d84cee
Daniel de Carvalho Fernandes <daniel.decarvalhofernandes@he-arc.ch>
2015-03-18 20:06:57 +0000

    Ajout de la page d'enregistrement et validation des champs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
   has_many :trajets, :class_name => 'trajets', :foreign_key => 'userAuteur_id'
   belongs_to :trajets, :class_name => 'trajets', :foreign_key => 'userBeneficiaire_id'
   
+   attr_accessor :password
+EMAIL_REGEX = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+  validates :pseudo, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+validates :email, :presence => true, :uniqueness => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+    message: "only allows letters" }
+ #password_confirmation attr
+validates_length_of :password, :in => 6..20, :on => :create 
+  
   
   
 end
