5a9e0a119a03d733c302158bbf98da14b33a7c95
Daniel de Carvalho Fernandes <daniel.decarvalhofernandes@he-arc.ch>
2015-03-26 15:48:53 +0000

    Ajout d'un trajet sur la google maps

--- app/models/trajet.rb
+++ app/models/trajet.rb
@@ -1,11 +1,16 @@
 class Trajet < ActiveRecord::Base
   belongs_to :users, :class_name => 'users', :foreign_key => 'id'
   has_many :users, :class_name => 'users', :foreign_key => 'id'
-  
-  geocoded_by :departVille, :latitude  => :latitude, :longitude => :longitude
-  before_validation :geocode
-  geocoded_by :arriveeVille, :latitude  => :latitudeArrivee, :longitude => :longitudeArrivee
-  after_validation :geocode
 
-  :geocode
+  before_save :saveCitiesCoord
+
+def saveCitiesCoord
+  city1 = Geocoder.coordinates(self.departVille)
+  city2 = Geocoder.coordinates(self.arriveeVille)
+   self.latitude = city1.first
+   self.longitude = city1.last
+   self.latitudeArrivee = city2.first
+   self.longitudeArrivee = city2.last
+end
+
 end
