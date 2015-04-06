204a778e0c42346a623180b87751a39963968f89
Spike1953 <kevin.baland@he-arc.ch>
2015-04-06 21:06:32 +0000

    test route Serveur

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :messages, only: [:new, :create]
   # You can have the root of your site routed with "root"
   #root 'home#index'
-  root 'buddycar#index'
+  root 'home#index'
   #resources :messages
 
   
